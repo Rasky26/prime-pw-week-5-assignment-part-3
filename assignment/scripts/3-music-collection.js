33fce7ac6a67bff395e9c7dd125693bae2bfd98f
Colin Oraskovich <Colin.Oraskovich@gmail.com>
2022-04-26 00:27:58 -0500

    Successfully created  function and added a record to the collection array.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,3 +2,18 @@ console.log('***** Music Collection *****')
 
 // Create an empty array for the `collection` variable
 let collection = []
+
+function addToCollection(title, artist, yearPublished) {
+    let record = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished,
+    }
+    collection.push(record)
+}
+
+console.log(collection)
+
+addToCollection('Bob', 'John', 1912)
+
+console.log(collection)
