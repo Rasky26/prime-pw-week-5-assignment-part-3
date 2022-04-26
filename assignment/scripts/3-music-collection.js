19c16a8fcf1e3b80baeb36cd714740b59e1e3593
Colin Oraskovich <Colin.Oraskovich@gmail.com>
2022-04-26 10:56:13 -0500

    Added six albums to the collection array. Logged out the array.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -4,16 +4,31 @@ console.log('***** Music Collection *****')
 let collection = []
 
 function addToCollection(title, artist, yearPublished) {
+    
+    // Using the imput, create a `record` object
     let record = {
         title: title,
         artist: artist,
         yearPublished: yearPublished,
     }
+
+    // Add the object to the `collection` array
     collection.push(record)
+
+    // Return the newly created object
+    return record
 }
 
-console.log(collection)
+// Check that the collection array is empty
+console.log('Starting with an empty `collection` array:', collection)
 
-addToCollection('Bob', 'John', 1912)
+// Add music to the collection - six in total
+console.log('Album added:', addToCollection('An Awesome Wave', 'Alt J', 2012))
+console.log('Album added:', addToCollection('Life in Cartoon Motion', 'Mika', 2007))
+console.log('Album added:', addToCollection('Stadium Arcadium', 'Red Hot Chili Peppers', 2006))
+console.log('Album added:', addToCollection('Californication', 'Red Hot Chili Peppers', 1999))
+console.log('Album added:', addToCollection('Ga Ga Ga Ga Ga', 'Spoon', 2007))
+console.log('Album added:', addToCollection('Plans', 'Death Cab for Cutie', 2005))
 
-console.log(collection)
+// Verify that all six albums were successfully added to the array
+console.log('Should contain 6-total albums:', collection)
